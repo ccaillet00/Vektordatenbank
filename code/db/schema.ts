0587559a65abe1dfc7d3c6dae2ea554b4e74b950
ccaillet00 <ccaillet@swissonline.ch>
2026-01-17 00:36:47 +0100

    added docs and update structure

--- code/db/schema.ts
+++ code/db/schema.ts
@@ -2,7 +2,7 @@ import { pgTable, serial, text, vector, index } from 'drizzle-orm/pg-core';
 
 export const knowledgeBase = pgTable('knowledge_base', {
   id: serial('id').primaryKey(),
-  content: text('content').notNull(), // Der Text deines Handbuchs
+  content: text('content').notNull(), // Text welcher wir ins embedding model schicken wollen
   embedding: vector('embedding', { dimensions: 1024 }), // Dein Vektor
 }, (table) => ({
   // Ein Index beschleunigt die Suche bei vielen Daten (HNSW ist top!)
