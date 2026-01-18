932f73bfb9350291dfbcc29893856d3aa34c572c
ccaillet00 <ccaillet@swissonline.ch>
2026-01-18 15:36:14 +0100

    added query for question

--- code/src/api/api.ts
+++ code/src/api/api.ts
@@ -2,6 +2,7 @@ import { type Request, type Response, type Express } from "express"
 import { knowledgeBase } from "../db/schema"
 import { db } from "../db/database"
 import { Ollama } from "ollama"
+import { cosineDistance, desc, gt, sql } from 'drizzle-orm'
 
 const ollama = new Ollama()
 
@@ -22,6 +23,29 @@ export const initializeVektorAPI = (app: Express) => {
         } catch (error) {
             console.log(error);
             res.status(500).send("Fehler beim Generieren des Embeddings");
+        }
+    })
+
+        app.post("/api/query", async (req: Request, res: Response) => {
+        try {
+            const { question } = req.body
+            
+            const getAnswer = await ollama.embeddings({
+                model: "mxbai-embed-large",
+                prompt: question,
+            });
+        const queryEmbedding = getAnswer.embedding
+        const similarity = sql<number>`1 - (${cosineDistance(knowledgeBase.embedding, queryEmbedding)})`;
+        const answer = await db
+        .select({ id: knowledgeBase.id, content: knowledgeBase.content, similarity})
+        .from(knowledgeBase)
+        .where(gt(similarity, 0.5))
+        .orderBy(desc(similarity))
+        .limit(4)
+        res.status(200).json(answer)
+        } catch (error) {
+            console.log(error)
+            res.status(500).send("Fehler beim abfragen ")
         }
     })
 }
